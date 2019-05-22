c123b146041cc1406d86e89a07f393971377c72f
ZakariyaF <zak.ferhi@gmail.com>
2019-05-22 19:17:16 +0100

    Fix DevLogDBContract SQL statement for creating a table

--- app/src/main/java/com/zakariyaf/DevLog/DevLogDBContract.java
+++ app/src/main/java/com/zakariyaf/DevLog/DevLogDBContract.java
@@ -16,7 +16,7 @@ public final class DevLogDBContract {
         public static final String COLUMN_COURSE_TITLE = "course_title";
 
         public static final String SQL_CREATE_TABLE =
-                "CREATE TABLE" + TABLE_NAME + " (" +
+                "CREATE TABLE " + TABLE_NAME + " (" +
                         _ID + " INTEGER PRIMARY KEY, " +
                         COLUMN_COURSE_ID + "TEXT UNIQUE NOT NULL, " +
                         COLUMN_COURSE_TITLE + "TEXT NOT NULL )";
@@ -31,7 +31,7 @@ public final class DevLogDBContract {
         public static final String COLUMN_COURSE_ID = "course_id";
 
         public static final String SQL_CREATE_TABLE =
-                "CREATE TABLE" + TABLE_NAME + " (" +
+                "CREATE TABLE " + TABLE_NAME + " (" +
                         _ID + " INTEGER PRIMARY KEY, " +
                         COLUMN_PROJECT_TITLE + " TEXT NOT NULL, " +
                         COLUMN_PROJECT_TEXT + " TEXT, " +
