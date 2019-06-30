e54c3104a88326b48dec2242ecc66b9b77b8d9b3
ZakariyaF <zak.ferhi@gmail.com>
2019-06-30 15:00:23 +0100

    Update the Spinner to load courses from the Content Provider

--- app/src/main/java/com/zakariyaf/DevLog/ProjectActivity.java
+++ app/src/main/java/com/zakariyaf/DevLog/ProjectActivity.java
@@ -5,6 +5,7 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
@@ -383,22 +384,14 @@ public class ProjectActivity extends AppCompatActivity implements LoaderManager.
     private CursorLoader createLoaderCourses() {
 
         mCoursesQueryFinished = false;
-
-        return new CursorLoader(this) {
-            @Override
-            public Cursor loadInBackground() {
-                SQLiteDatabase db = mDbOpenHelper.getReadableDatabase();
-                String[] courseColumns = {
-                        CourseInfoEntry.COLUMN_COURSE_TITLE,
-                        CourseInfoEntry.COLUMN_COURSE_ID,
-                        CourseInfoEntry._ID
-                };
-                return db.query(CourseInfoEntry.TABLE_NAME, courseColumns,
-                        null, null, null, null,
-                        CourseInfoEntry.COLUMN_COURSE_TITLE
-                );
-            }
+        Uri uri = Uri.parse("content://com.zakariyaf.devlog.provider");
+        String[] courseColumns = {
+                CourseInfoEntry.COLUMN_COURSE_TITLE,
+                CourseInfoEntry.COLUMN_COURSE_ID,
+                CourseInfoEntry._ID
         };
+        return new CursorLoader(this, uri, courseColumns, null, null,
+                CourseInfoEntry.COLUMN_COURSE_TITLE);
     }
 
     @Override
