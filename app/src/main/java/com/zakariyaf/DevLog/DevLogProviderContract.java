e7ba8a0b07c41d5bbe027d45ce03e8d92d0ecbcf
ZakariyaF <zak.ferhi@gmail.com>
2019-07-01 14:21:37 +0100

    Add table columns to the provider contract class

--- app/src/main/java/com/zakariyaf/DevLog/DevLogProviderContract.java
+++ app/src/main/java/com/zakariyaf/DevLog/DevLogProviderContract.java
@@ -1,6 +1,7 @@
 package com.zakariyaf.DevLog;
 
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 public final class DevLogProviderContract {
 
@@ -10,12 +11,25 @@ public final class DevLogProviderContract {
     private DevLogProviderContract() {
     }
 
-    public static final class Courses {
+    protected interface CoursesIdColumns {
+        public static final String COLUMN_COURSE_ID = "course_id";
+    }
+
+    protected interface CoursesColumns {
+        public static final String COLUMN_COURSE_TITLE = "course_title";
+    }
+
+    protected interface ProjectsColumns {
+        public static final String COLUMN_PROJECT_TITLE = "project_title";
+        public static final String COLUMN_PROJECT_TEXT = "project_text";
+    }
+
+    public static final class Courses implements CoursesColumns, CoursesIdColumns, BaseColumns {
         public static final String PATH = "courses";
         public static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, PATH);
     }
 
-    public static final class Projects {
+    public static final class Projects implements ProjectsColumns, CoursesIdColumns, BaseColumns {
         public static final String PATH = "projects";
         public static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, PATH);
     }
